ce979239df45bb7e10b271227d0d4c7c113a40d9
Graycemuthui <graycemuthui20@gmail.com>
2022-05-09 18:19:22 +0300

    Add hamburger to the nav-links

---
+++ index.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelector(".nav-links");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".fa-times");
+const menuIcon = document.querySelector(".fa-bars");
+
+
+function toggleMenu() {
+  if (navLinks.classList.contains("showMenu")) {
+    navLinks.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    navLinks.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
