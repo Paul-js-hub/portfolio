c69bba25b6fe63d73d8e8d487150d976cfb7260d
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-10 09:48:56 +0300

    Fix linter errors

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const navLinks = document.querySelector(".nav-links");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".fa-times");
-const menuIcon = document.querySelector(".fa-bars");
-
+const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.fa-times');
+const menuIcon = document.querySelector('.fa-bars');
 
 function toggleMenuBar() {
-  if (navLinks.classList.contains("show-menu")) {
-    navLinks.classList.remove("show-menu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (navLinks.classList.contains('show-menu')) {
+    navLinks.classList.remove('show-menu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    navLinks.classList.add("show-menu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    navLinks.classList.add('show-menu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
 }
 
-hamburger.addEventListener("click", toggleMenuBar);
+hamburger.addEventListener('click', toggleMenuBar);
 
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenuBar);
-  }
-)
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenuBar);
+});
