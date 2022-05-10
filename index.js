fd9fc2a39ff85830f7b948a0d60b5234eb154080
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-10 09:22:07 +0300

    Add functionality when a user clicks the mobile options, the mobile menu disappears

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
